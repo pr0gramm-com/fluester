963e90182d2923674c56ed3ab825b94c1670b4d7
holzmaster <holzmaster@outlook.com>
2023-10-09 21:11:25 +0200

    Fix index file name

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,142 @@
-import path from "node:path";
+import { existsSync } from "node:fs";
+import * as path from "node:path";
 
 import { execute } from "./execute.js";
-import type { ModelName } from "./model.js";
+import { defaultExecutablePath } from "./interop.js";
+import { ModelName, modelFileNames } from "./model.js";
 import transcriptToArray, { TranscriptLine } from "./tsToArray.js";
-import { FlagTypes, buildExecCommand } from "./whisper.js";
+
+export interface WhisperClientOptions {
+	/** Path to the whisper executable */
+	executablePath?: string;
+	/** Path to the whisper models */
+	// modelsPath: string;
+}
 
 export interface WhisperOptions {
 	modelName?: string; // name of model stored in node_modules/@pr0gramm/fluester/lib/whisper.cpp/models
 	modelPath?: string; // custom path for model
 	whisperOptions?: FlagTypes;
+	// TODO: AbortSignal support
 }
 
-/**
- * @param filePath Path to audio file.
- * @param options Whisper options.
- * @throws Some error if execution failed.
- */
-export async function whisper(
-	filePath: string,
-	options?: WhisperOptions,
-): Promise<TranscriptLine[]> {
-	try {
-		// todo: combine steps 1 & 2 into separate function called whisperCpp (createCppCommand + shell)
+export interface WhisperClient {
+	translate: (filePath: string, options: WhisperOptions) => Promise<TranscriptLine[]>;
+}
 
-		// 1. create command string for whisper.cpp
-		const command = buildExecCommand({
-			filePath: path.normalize(filePath),
-			modelName: options?.modelName as ModelName,
-			modelPath: options?.modelPath,
-			options: options?.whisperOptions,
-		});
+export function createWhisperClient(
+	options: WhisperClientOptions,
+): WhisperClient {
+	const effectiveOptions = {
+		executablePath: defaultExecutablePath,
+		...options,
+	};
 
-		// 2. run command in whisper.cpp directory
-		// todo: add return for continually updated progress value
-		const transcript = await execute(...command);
+	return {
+		// TODO
+		translate: async (filePath: string, options?: WhisperOptions) => {
+			try {
+				// 1. create command string for whisper.cpp
+				const args = buildCliArgs({
+					filePath: path.normalize(filePath),
+					modelName: options?.modelName as ModelName,
+					modelPath: options?.modelPath,
+					options: options?.whisperOptions,
+				});
 
-		// 3. parse whisper response string into array
-		return transcriptToArray(transcript.toString());
-	} catch (cause) {
-		throw new Error("Error during whisper operation", { cause });
+				// 2. run command in whisper.cpp directory
+				// todo: add return for continually updated progress value
+				const transcript = await execute(effectiveOptions.executablePath, args);
+
+				// 3. parse whisper response string into array
+				return transcriptToArray(transcript.toString());
+			} catch (cause) {
+				throw new Error("Error during whisper operation", { cause });
+			}
+		},
+	};
+
+	function buildCliArgs({
+		filePath,
+		modelName = undefined,
+		modelPath = undefined,
+		options = { wordTimestamps: true },
+	}: CppCommandTypes) {
+		const model = modelPathOrName(modelName, modelPath);
+		return [...getFlags(options), "-m", model, "-f", filePath];
 	}
+}
+
+function modelPathOrName(
+	modelName: ModelName | undefined,
+	modelPath: string | undefined,
+) {
+	if (modelName && modelPath) {
+		throw new Error("Submit a modelName OR a modelPath. NOT BOTH!");
+	}
+	if (!modelName && !modelPath) {
+		throw new Error("No 'modelName' or 'modelPath' provided");
+	}
+
+	if (modelPath) {
+		return modelPath;
+	}
+
+	if (!modelName || !(modelName in modelFileNames)) {
+		throw new Error(`Invalid model name: "${modelName}"`);
+	}
+
+	const correspondingPath = `./models/${modelFileNames[modelName]}`;
+	if (!existsSync(correspondingPath)) {
+		throw new Error(
+			`"${modelName}" not found. Run "npx @pr0gramm/fluester download"`,
+		);
+	}
+	return correspondingPath;
+}
+
+// option flags list: https://github.com/ggerganov/whisper.cpp/blob/master/README.md?plain=1#L91
+function getFlags(flags: FlagTypes): string[] {
+	const s = [];
+
+	// output files
+	if (flags.generateTxt) {
+		s.push("-otxt");
+	}
+	if (flags.generateSubtitles) {
+		s.push("-osrt");
+	}
+	if (flags.generateVtt) {
+		s.push("-ovtt");
+	}
+
+	// timestamps
+	if (flags.timestampSize) {
+		s.push("-ml");
+		s.push(flags.timestampSize.toString());
+	}
+	if (flags.wordTimestamps) {
+		s.push("-ml");
+		s.push("1");
+	}
+
+	return s;
+}
+
+export interface CppCommandTypes {
+	filePath: string;
+	modelName?: ModelName;
+	modelPath?: string;
+	options?: FlagTypes;
+}
+
+export interface FlagTypes {
+	/** Build TXT? */
+	generateTxt?: boolean;
+	/** Build SRT? */
+	generateSubtitles?: boolean;
+	/** Build VTT? */
+	generateVtt?: boolean;
+	timestampSize?: number;
+	wordTimestamps?: boolean;
 }
