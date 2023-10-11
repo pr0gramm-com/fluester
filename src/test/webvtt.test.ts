3e62dfb361269a0d34209ff4f6019a59f5a77f89
SoraTenshi <dream@neoncity.dev>
2023-10-12 01:18:20 +0200

    Fix imports

--- src/test/webvtt.test.ts
+++ src/test/webvtt.test.ts
@@ -1,8 +1,6 @@
 import * as assert from "node:assert/strict";
 import { describe, test } from "node:test";
 import { createWebVttString } from "../webvtt.js";
-import type { TranscriptLine } from "../transcript.js";
-import { AssertionError } from "node:assert";
 
 describe("webvtt", () => {
 	const basic_ok = `WEBVTT
