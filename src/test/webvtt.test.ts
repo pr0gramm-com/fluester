131023fb79c54ba3ded530da0e979b5c4537f581
SoraTenshi <dream@neoncity.dev>
2023-10-12 01:10:08 +0200

    Fix test formatting

--- src/test/webvtt.test.ts
+++ src/test/webvtt.test.ts
@@ -10,13 +10,16 @@ describe("webvtt", () => {
 00:00:00.000 --> 00:00:01.000
 Hello`;
 	test("basic ok", () => {
-		assert.deepStrictEqual(createWebVttString([
-			{
-				start: "00:00:00.000",
-				end: "00:00:01.000",
-				speech: "Hello",
-			},
-		]), basic_ok);
+		assert.deepStrictEqual(
+			createWebVttString([
+				{
+					start: "00:00:00.000",
+					end: "00:00:01.000",
+					speech: "Hello",
+				},
+			]),
+			basic_ok,
+		);
 	});
 
 	const multiple_ok = `WEBVTT
@@ -31,42 +34,49 @@ World
 !`;
 
 	test("multiple ok", () => {
-		assert.deepStrictEqual(createWebVttString([
-			{
-				start: "00:00:00.000",
-				end: "00:00:01.000",
-				speech: "Hello",
-			},
-			{
-				start: "00:00:02.000",
-				end: "00:00:03.000",
-				speech: "World",
-			},
-			{
-				start: "00:00:04.000",
-				end: "00:00:05.000",
-				speech: "!",
-			}
-		]), multiple_ok);
+		assert.deepStrictEqual(
+			createWebVttString([
+				{
+					start: "00:00:00.000",
+					end: "00:00:01.000",
+					speech: "Hello",
+				},
+				{
+					start: "00:00:02.000",
+					end: "00:00:03.000",
+					speech: "World",
+				},
+				{
+					start: "00:00:04.000",
+					end: "00:00:05.000",
+					speech: "!",
+				},
+			]),
+			multiple_ok,
+		);
 	});
 
 	test("invalid timestamps", () => {
-		assert.throws(() => { 
-			createWebVttString([{
-				start: "aaaa",
-				end: "bbbbb",
-				speech: "fail",
-			}]);
+		assert.throws(() => {
+			createWebVttString([
+				{
+					start: "aaaa",
+					end: "bbbbb",
+					speech: "fail",
+				},
+			]);
 		}, "throws assertion error");
 	});
 
 	test("negative end-start", () => {
-		assert.throws(() => { 
-			createWebVttString([{
-				start: "00:01:00.000",
-				end: "00:00:01.000",
-				speech: "fail",
-			}]);
+		assert.throws(() => {
+			createWebVttString([
+				{
+					start: "00:01:00.000",
+					end: "00:00:01.000",
+					speech: "fail",
+				},
+			]);
 		}, "throws assertion error");
 	});
 });
