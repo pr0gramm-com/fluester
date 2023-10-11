f609c22523c1852e87e5feb21f08a8d4590dbba5
SoraTenshi <dream@neoncity.dev>
2023-10-11 23:33:03 +0200

    Add asserts

--- src/webvtt.ts
+++ src/webvtt.ts
@@ -1,9 +1,14 @@
+import assert from "node:assert";
 import { TranscriptLine } from "./transcript";
 
 export function createWebVttString(transcript: TranscriptLine[]): string {
   const builder = ["WEBVTT", "\n"];
 
   for(const {start, end, speech} of transcript) {
+    assert(start <= end, "start <= end");
+    assert("00:00:00.000".length == start.length, "00:00:00.000.length == start.length");
+    assert("00:00:00.000".length == end.length, "00:00:00.000.length == end.length");
+
     builder.push(`${start} --> ${end}`);
     for(const split of speech.split("\n")) {
       builder.push(`${split}`);
