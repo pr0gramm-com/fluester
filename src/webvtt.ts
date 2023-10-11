849028b5909139ce819cf9716944f7f4c6f6eaea
SoraTenshi <dream@neoncity.dev>
2023-10-12 01:15:47 +0200

    Change asserts to returning errors

--- src/webvtt.ts
+++ src/webvtt.ts
@@ -1,19 +1,24 @@
-import assert from "node:assert";
 import type { TranscriptLine } from "./transcript.js";
 
+/**
+ * @throws If a timestamp has an invalid length
+ * @throws If start > end
+ */
 export function createWebVttString(transcript: TranscriptLine[]): string {
 	const builder = ["WEBVTT", ""];
 
 	for (const { start, end, speech } of transcript) {
-		assert(start <= end, "start <= end");
-		assert(
-			"00:00:00.000".length === start.length,
-			"00:00:00.000.length == start.length",
-		);
-		assert(
-			"00:00:00.000".length === end.length,
-			"00:00:00.000.length == end.length",
-		);
+		if (start > end) {
+			throw new Error("start > end");
+		}
+
+		if ("00:00:00.000".length !== start.length) {
+			throw new Error("00:00:00.000.length !== start.length");
+		}
+
+		if ("00:00:00.000".length !== end.length) {
+			throw new Error("00:00:00.000.length !== end.length");
+		}
 
 		builder.push(`${start} --> ${end}`);
 		for (const split of speech.split("\n")) {
