15715b2d518518c7b8d7f9bfc1b3b386fec353ec
SoraTenshi <dream@neoncity.dev>
2023-10-12 01:07:59 +0200

    Add tests

--- src/webvtt.ts
+++ src/webvtt.ts
@@ -1,21 +1,26 @@
 import assert from "node:assert";
-import { TranscriptLine } from "./transcript";
+import type { TranscriptLine } from "./transcript.js";
 
 export function createWebVttString(transcript: TranscriptLine[]): string {
-  const builder = ["WEBVTT", "\n"];
+	const builder = ["WEBVTT", ""];
 
-  for(const {start, end, speech} of transcript) {
-    assert(start <= end, "start <= end");
-    assert("00:00:00.000".length == start.length, "00:00:00.000.length == start.length");
-    assert("00:00:00.000".length == end.length, "00:00:00.000.length == end.length");
+	for (const { start, end, speech } of transcript) {
+		assert(start <= end, "start <= end");
+		assert(
+			"00:00:00.000".length === start.length,
+			"00:00:00.000.length == start.length",
+		);
+		assert(
+			"00:00:00.000".length === end.length,
+			"00:00:00.000.length == end.length",
+		);
 
-    builder.push(`${start} --> ${end}`);
-    for(const split of speech.split("\n")) {
-      builder.push(`${split}`);
-    }
-    builder.push("\n");
-  }
+		builder.push(`${start} --> ${end}`);
+		for (const split of speech.split("\n")) {
+			builder.push(`${split}`);
+			builder.push("");
+		}
+	}
 
-  return builder.join("\n");
+	return builder.join("\n").trim();
 }
-
